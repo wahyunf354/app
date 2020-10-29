cdc4855aea475b8a11631be424050470bd022cbf
wahyunf354 <wahyunurfadillah313@gmail.com>
2020-10-29 12:40:52 +0700

    init project and insert 3 data

---
+++ data.js
@@ -0,0 +1,117 @@
+/**
+ * module data berisi data dari tanaman-tanaman obat berserta khasiatnya
+ *
+ * Untuk schema data sebadai berikut
+ * {
+ *    id: int,
+ *    nama: String,
+ *    namaLatin: String,
+ *    desc: String,
+ *    imgUrl: String,
+ *    habitat: String,
+ *    caraBudidaya: String,
+ *    namaDaerah: [ String ],
+ *    bagianYangMenjadiObat: [ String ],
+ *    ujiKlinik: String,
+ *    khasiat: String,
+ *    komposisi: String,
+ *    ramuan: [ String ],
+ *    caraPemakaian: String,
+ *    lamaPengobatan: String
+ * }
+ */
+
+module.export = [
+  {
+    id: 1,
+    nama: "Alang-Alang",
+    namaLatin: "Imperata cylindrical (L.) Beauv.var.mayor",
+    imgUrl: "img/alang-alang.jpg",
+    desc:
+      "Alang-alang (Imperata cylindrical (L.) Beauv.var.mayor ), Perawakan: herba, rumput, merayap, tinggi 30-180 cm. Batang: rimpang, merayap di bawah tanah, batang tegak membentuk satu perbungaan, padat, pada bukunya berambut jarang. Daun: tunggal, pangkal saling menutup, helaian; berbentuk pita, ujung runcing tajam, tegak, kasar, berambut jarang, ukuran 12-80 cm. x 35-18 cm. Bunga: susunan majemuk bulir majemuk, agak menguncup, panjang 6-28 cm, setiap cabang memiliki 2 bulir, cabang 2,5-5 cm, tangkai bunga 1-3 mm, gluma 1; ujung bersilia, 3-6 urat, Lemma 1 (sekam); bulat telur melebar, silia pendek 1,5-2,5 mm. Lemma 2 (sekam); memanjang, runcing 0,5-2,5 mm. Palea (sekam); 0,75-2 mm. Benang sari: kepala sari 2,5-3,5 mm, putih kekuningan atau ungu. Putik: kepala putik berbentuk bulu ayam. Buah: tipe padi. Biji: berbentuk jorong, panjang 1 mm lebih. Waktu berbunga : Januari – Desember. Daerah distribusi, Habitat dan Budidaya: Di Jawa tumbuh pada ketinggian sampai dengan 2700 m dpl, pada daerah-daerah terbuka atau setengah tertutup; rawa-rawa; pada tanah dengan aerasi yang baik; pada daerah-daerah yang habis dibuka; di tepi sungai; ekstensif pada hutan sekunder; daerah bekas terbakar; sebagai gulma di perladangan; taman dan perkebunan. Tumbuhan ini dapat mempengaruhi tanaman kultivasi lain, karena kebutuhan natrium yang relatif tinggi.",
+    habitat:
+      "Berkembang biak dengan sendirinya. Setiap saat rimpang dipanen dari tumbuhan yang telah matang. Rimpang yang baik berwarna pucat, berasa manis dan sejuk. Alang-alang dapat menyebabkan penurunan pH tanah. Besarnya penurunan pH dan ham-batan terhadap proses nitrifikasi menunjukkan adanya korelasi positif dengan pertumbuhan alang-alang. Bagian yang digunakan untuk obat medis adalah akarnya. Kenyataannya, akarnya dapat digunakan untuk menurunkan temperatur, melancarkan urin, menghentikan pendarahan, dan sebagai obat untuk pendarahan pada hidung, memuntahkan darah, gonorea (kencing nanah), hepatitis, infeksi ginjal. Penelitian menemukan bahwa alang-alang mengandung 19 mannitol, glukosa, asam malic, asam sitrat, coixol, arundoin, silindrin, fernerol, simiarenol, anemonin, esin, alkali, saponin, taninin, dan polifenol.",
+    caraBudidaya:
+      "Perbanyakan dengan rimpang atau akar tinggal. Tanaman ini sangat mudah tumbuh dan terdapat dimanamana disekitar kita. Merupakan tumbuhan liar dan menjurus sebagai gulma. Tumbuh liar di pinggir jalan, di 21 ladang dan di hutan. Tumbuhan ini termasuk terna menahun, tinggi dapat mencapai 180 cm. Batang padat, buku berambut jarang. Daun berbentuk pita, berwarna hijau, permukaan daun kasar. Perbungaan berupa bulir, warna putih, bunga yang terletak di bagian atas adalah bunga sempurna dan yang terletak di bawah adalah bunga mandul. Bunga mudah diterbangkan oleh angin.",
+    namaDaerah: [
+      "Naleueng lakoe (Aceh)",
+      "Jih (Gayo)",
+      "Alalang, Hilalang, Ilalang (Minang kabau)",
+      "Lioh (Lampung)",
+      "Halalang, Tingen, Padang, Tingan, Puang, Buhang, Belalang, Bolalang (Dayak)",
+      "Eurih (Sunda)",
+      "Alang-alang kambengan (Jawa)",
+      "Kebut, Lalang (Madura)",
+      "Ambengan, Lalang (BaIi)",
+      "Kii, Rii (FIores)",
+      "Padengo, Padanga (Gorontalo)",
+      "Deya (Bugis)",
+      "Erer, Muis, Wen (Seram)",
+      "Weli, Welia, Wed (Ambon)",
+      "Rih, Ri (Batak)",
+      "Oo (Nias)",
+    ],
+    bagianYangMenjadiObat: ["Akar", "rimpang (daun)", "bunga"],
+    ujiKlinik:
+      "Dekokta akar alang-alang dengan dosis 250-300 g, 2 kali pagi dan sore dapat menyembuhkan 27 kasus dari 30 penderita nefritis akut. Pada nefritis kronis, herba alangalang dapat mengurangi edema dan menurunkan tekanan darah. Dekokta herba 250 g dalam bentuk tunggal maupun dikombinasikan dengan rimpang dan daun Nelumbo nucifera dan daun Agrimonia pilosa dapat mengobati epistaksis (mimisan), hemoptisis (batuk darah), hematuri (kencing darah), menorrhagia, dan perdarahan gastrointestinal bagian atas. Di samping itu dilaporkan juga bahwa dekokta akar alang-alang dapat efektif untuk pengobatan hepatitis viral akut pada 28 kasus; biasanya digunakan bersama-sama dengan Plantago asiatica, Glechoma longituba dan tunas Artemisia capillaris. Toksisitas: Pada pemakaian sesuai aturan, praktis tidak toksik. Efek yang tidak dfinginkan: Pusing, mual, adanya peningkatan rasa ingin buang air besar, kadang-kadang terjadi pada penggunaan klinik. Teknologi Farmasi: Selulosa daun alang-alang mempunyai daya serap terhadap air yang relatif cukup baik dalam pembuatan tablet secara cetak langsung.",
+    khasiat:
+      "Rimpang: pelembut kulit; peluruh air seni, pembersih darah, penambah nafsu makan, penghenti perdarahan. di samping itu dapat digunakan pula dalam upaya pengobatan penyakit kelamin (kencing nanah, kencing darah, raja singa), penyakit ginjal, luka, demam, tekanan darah tinggi dan penyakit syaraf. Semua bagian tumbuhan digunakan sebagai pakan hewan,bahan kertas,dan untuk pengobatan kurap.",
+    komposisi:
+      "Akar: metabolit yang telah ditemukan pada akar alangalang terdiri dari arundoin, fernenol, isoarborinol, silindrin, simiarenol, kampesterol, stigmasterol, ßsitosterol, skopoletin, skopolin, p-hidroksibenzaladehida, katekol, asam klorogenat, asam isoklorogenat, asam pkumarat, asam neoklorogenat, asam asetat, asam oksalat, asam d-malat, asam sitrat, potassium (0,75% dari berat kering), sejumlah besar kalsium dan 5- hidroksitriptamin. Dari hasil penelitian lain terhadap akar dan daun ditemukan 5 macam turunan flavonoid yaitu turunan 3′,4′,7-trihidroksi flavon, 2′,3′-dihidroksi kalkon dan 6-hidroksi flavanol. Suatu turunan flavonoid yang kemungkinan termasuk golongan flavon, flavonol tersubstitusi pada 3-0H, flavanon atau isoflavon terdapat pada fraksi ekstrak yang larut dalam etilasetat akar alang-alang. Pada fraksi ekstrak yang larut dalam air akar alang-alang ditemukan golongan senyawa flavon tanpa gugus OH bebas, flavon, flavonol tersubstitusi pada 3- 0H, flavanon, atau isoflavon.",
+    ramuan: [
+      "Rimpang Alang-alang 6 gram Daun sendok segar 6 gram Daun Andong segar 2 helai Air 110 ml.",
+    ],
+    caraPemakaian:
+      "Diminum 2 kali sehari, pagi dan sore, tiap kali minum 100 ml. (untuk infus). Untuk pipisan diminum 2 kali sehari, pagi dan sore, tiap kali minum 1/4 cangkir. Untuk pil diminum 3 kali sehari 9 pil. LAMA PENGOBATAN Diulang sampai sembuh.",
+    lamaPengobatan: "Sampai Sembuh",
+  },
+  {
+    id: 2,
+    nama: "Alpokat",
+    namaLatin: "Persea gratissima Gaerin",
+    imgUrl: "img/alpokat.jpg",
+    desc:
+      "Pohon, tinggi dapat mencapai 10 meter, batang berkayu, dan bercabang-cabang. daun tunggal berbentuk bulat telur dan berwarna hijau. Perbungaan berbentuk malai, tumbuh di ujung ranting. Buah buni bentuk bulat telur, bentuk pita atau bentuk bulat. Warna buah hijau sampai ungu. Daging buah jika sudah masak berwarna kuning atau kkuning kehijauan. Akar termasuk akar tunggang 11 HABITAT Tumbuh pada daerah berikilim panas pada dataran rendah sampai ketinggian 1200 m dpl ",
+    habitat: null,
+    caraBudidaya: null,
+    namaDaerah: ["Apukat", "Advocaat", "Alpokat", "Apuket"],
+    bagianYangMenjadiObat: ["Daun"],
+    ujiKlinik: null,
+    khasiat: "Diuretik; Anti bakteri Nama simplesia: Perseae Folium",
+    komposisi: null,
+    ramuan: [
+      "Batu ginjal : Daun avokat segar 7 helai; Air 110 ml, Dibuat infus atau diseduh, Diminum sehari 2 kali; pagi dan sore, tiap kali minum 100 ml ",
+      "Sakit perut dan Disentri : Daun avokat segar 5 g; Rimpang temu kunci segar 5 g; Rimpang kunyit segar 6 g; Rasuk angin 1/2 g; Daun pegagan segar 6 g; Air 115 ml, Dibuat infus atau diseduh, Diminum 1 kali sehari 100 ml.",
+    ],
+    caraPemakaian: null,
+    lamaPengobatan: null,
+  },
+  {
+    id: 3,
+    nama: "Belimbing Wuluh",
+    namaLatin: "Averrhoa bilimbi L",
+    imgUrl: "img/blimbing-wuluh.jpg",
+    desc:
+      "Tumbuhan berbatang keras, tinggi mencapi 11 meter, daun bersirip genap. Batang tidak bercabang. Bunga berbentuk bintang, berwarna merah muda sampai ungu. Buah beruang 5, bergantung pada batang atau dahan. Buah berair dan berasa asam. ",
+    habitat:
+      "Tumbuh liar atau dibudidayakan di pekarangan yang cukup memperoleh sinar matahari.  ",
+    caraBudidaya: null,
+    namaDaerah: [
+      "Belimbing wuluh",
+      "Belimbing buloh",
+      "Belimbing asam",
+      "Belimbing tunjuk",
+      "Balimbeng",
+    ],
+    bagianYangMenjadiObat: ["Daun", "Bunga", "Buah"],
+    ujiKlinik: null,
+    khasiat: "Antipiretik, Ekspektoran ",
+    komposisi: null,
+    ramuan: [
+      "Sakit tenggorokan; Sariawan: Bunga belimbing wuluh segar 1 genggam; Buah adas manis secukupnya; Air 1/4 cangkir; Gula batu secukupnya, Dipipis, Diminum sehari 2 kali; pagi dan sore; tiap kali diminum 1 sampai 2 sendok makan.",
+      "Kencing manis: Daun belimbing wuluh segar 20 g; Air secukupnya, Dipipis, Diminum 2 kali sehari; pagi dan sore; tiap kali minum 1/4 cangkir.",
+    ],
+    caraPemakaian: null,
+    lamaPengobatan: null,
+  },
+];
