ddea18a22abd1f5efdbdb36d5904dc1431bf565b
wahyunf354 <wahyunurfadillah313@gmail.com>
2020-11-23 15:47:45 +0700

    dicoba buat modal belum berhasil

--- src/script/main.js
+++ src/script/main.js
@@ -15,24 +15,8 @@ document.addEventListener("DOMContentLoaded", () => {
           const itemTanaman = document.createElement("div");
           itemTanaman.setAttribute("class", "item-tanaman");
           itemTanaman.innerHTML = `
-                <div class="card  shadow-lg">
-                  <img src="/src/img/${
-                    result.imgUrl
-                  }" class="card-img-top rounded" style="width: 100%;"
-                       alt="...">
-                  <div class="card-body">
-                    <h2 class="card-title display-1">${result.nama}</h2>
-                    <p class="card-subtitle mb-2 text-muted h1" style="color: #bbb">${
-                      result.namaLatin
-                    }</p>
-                    <p class="card-text">
-                      ${result.ramuan.reduce(
-                        (acc, cv) => acc + "<br><br>" + cv
-                      )}
-                    </p>
-                  </div>
-                </div>
-                `;
+                  <h5 data-toggle="modal" data-target="#exampleModal">${result.nama}</h5>
+                  `;
           listTanaman.appendChild(itemTanaman);
         });
       })
@@ -40,6 +24,28 @@ document.addEventListener("DOMContentLoaded", () => {
         listTanaman.innerHTML = message;
       });
   };
+
+  const displayModal = (data) => {
+    const html = ` <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <img src="/src/img/${data.imgUrl}" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">${data.nama}</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          </div>
+        
+        </div>
+      </div>
+    </div>` 
+    return html;
+  }
 
   searchElement.addEventListener("keyup", onSearchButtonClicked);
   searchButton.addEventListener("click", onSearchButtonClicked);
